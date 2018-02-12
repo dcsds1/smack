efdca1dac47e0da6d9d6dbfdc4969c74dde34144
dcsds1 <dcsds1@sina.com>
2018-02-12 14:11:15 +0800

    finish login page

--- smack.py
+++ smack.py
@@ -9,6 +9,12 @@ app.config.from_object(config)
 def index():
     return render_template('index.html')
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        return
 
 if __name__ == '__main__':
     app.run()
