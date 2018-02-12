46b56739d481d4f44fe13e3b879697f08e7766bb
dcsds1 <dcsds1@sina.com>
2018-02-12 15:03:48 +0800

    finish register page

--- manager.py
+++ manager.py
@@ -2,13 +2,13 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from smack import app
 from exts import db
-
+from models import User
 
 manager = Manager(app)
 
 migrate = Migrate(app, db)
 
-manager.add_command('db', Migrate)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,1 +1,7 @@
 from exts import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
